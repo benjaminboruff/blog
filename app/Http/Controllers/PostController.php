0f744a55f0a33a0afc85cfd6e4646912d78c4a43
Benjamin Boruff <benboruff@gmail.com>
2016-04-09 19:15:43 -0400

    create post method in PostContoller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,6 @@ class PostController extends Controller
 {
     // show 5 posts with pagination
     // controls root requests
-
     public function index()
     {
       // retrieve 5 posts from DB that are active and recent
@@ -23,5 +22,17 @@ class PostController extends Controller
       $title = 'Recent Posts';
 
       return view('home')->withPosts($posts)->withTitle($title);
-    }
+    };
+
+    // return post form to user if permissions allow
+    public function create(Request $request)
+    {
+      // if user is author or admin role they can post
+      if($request->user()->can_post())
+      {
+        return view('create_post');
+      } else {
+        return redirect('/')->withErrors('You are not autorized to post');
+      }
+    };
 }
