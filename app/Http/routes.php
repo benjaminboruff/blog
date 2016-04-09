0c1afb8ded626319c6d1cd6096d70e56bf894016
Benjamin Boruff <benboruff@gmail.com>
2016-04-08 20:48:35 -0400

    routes fleshed out

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,3 +15,35 @@ Route::get('/', 'PostController@index');
 Route::get('/home', 'PostController@index');
 
 Route::auth();
+
+// logged-in-only routes
+Route::group(['middleware' => ['auth']], function()
+{
+  // display new post form
+  Route::get('new-post', 'PostController@create');
+  // save new post
+  Route::post('new-post', 'PostController@store');
+  // edit post form
+  Route::get('edit/{slug}', 'PostController@edit');
+  // update post
+  Route::post('update', 'PostController@update');
+  // delete post
+  Route::get('delete/{id}', 'PostController@delete');
+
+  // display all posts for user
+  Route::get('posts', 'UserController@posts');
+  // display user's drafts
+  Route::get('drafts', 'UserController@drafts');
+
+  // add comment to post
+  Route::post('comment/add', 'CommentController@store');
+  // delete comment
+  Route::post('comment/delete/{id}', 'CommentController@delete');
+});
+
+// get user's profile
+Route::get('user/{id}', 'UserController@profile')->where('id', '[0-9]+');
+// display list of posts
+Route::get('user/{id}/posts', 'UserController@list_posts')->where('id', '[0-9]+');
+// display a post
+Route::get('/{slug}', 'PostController@show');
